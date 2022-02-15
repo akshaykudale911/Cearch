3398b4518806dde64ae30535797319116f43a07e
Akshay Kudale <akshaykudale911@gmail.com>
2022-02-15 14:22:23 +0530

    Base console app

---
+++ Search Text/Search Text/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search_Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Search Text Program");
+            Console.ReadLine();
+        }
+    }
+}
